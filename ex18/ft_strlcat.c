8fd2f31c73b81e57b0860305968881bfa178a0fc
Sen Li <seli@e1z1r7p16.42.us.org>
2018-09-25 00:56:01 -0700

    strlcat

---
+++ ex18/ft_strlcat.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: seli <seli@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/09/25 00:45:47 by seli              #+#    #+#             */
+/*   Updated: 2018/09/25 00:55:41 by seli             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
+{
+	char	*head;
+	int		dstlen;
+	int		max;
+
+	dstlen == ft_strlen(dest);
+	head = dest;
+	max = size - dstlen - 1;
+	while (*head)
+	{
+		head++;
+		max--;
+	}
+	while (*src && max > 0)
+	{
+		*head++ = *src++;
+		max--;
+	}
+	if (!(size == 0 || dstlen > size))
+		*head = 0;
+	return (dest);
+}
+
+int				ft_strlen(char *str)
+{
+	int len;
+
+	len = 0;
+	while (*str)
+	{
+		len++;
+		str++;
+	}
+	return (len);
+}
