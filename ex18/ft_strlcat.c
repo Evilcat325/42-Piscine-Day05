73fcb164c40966dbff37ebd2bef5d150dd094b2e
Sen Li <seli@e1z1r7p16.42.us.org>
2018-09-25 02:30:26 -0700

    percise mimick strlen behavior

--- ex18/ft_strlcat.c
+++ ex18/ft_strlcat.c
@@ -6,43 +6,42 @@
 /*   By: seli <seli@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/25 00:45:47 by seli              #+#    #+#             */
-/*   Updated: 2018/09/25 01:18:52 by seli             ###   ########.fr       */
+/*   Updated: 2018/09/25 02:29:57 by seli             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int				ft_strlen(char *str);
+#include <stdio.h>
+
+unsigned int	ft_strlen(char *str);
 
 unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
 {
-	char	*head;
-	int		dstlen;
-	int		srclen;
-	int		max;
+	char			*head;
+	int				max;
+	unsigned int	dstlen;
+	unsigned int	srclen;
 
-	dstlen = 0;
 	srclen = 0;
+	dstlen = 0;
 	srclen = ft_strlen(src);
 	dstlen = ft_strlen(dest);
-	head = dest;
-	max = (int)size - dstlen - 1;
-	while (*head)
-	{
-		head++;
-		max--;
-	}
+	head = dest + dstlen;
+	max = size - dstlen - 1;
+	if (max < 0)
+		return (size + srclen);
 	while (*src && max > 0)
 	{
 		*head++ = *src++;
 		max--;
 	}
-	if (!(size == 0 || dstlen > (int)size))
+	if (!(size == 0 || dstlen > size))
 		*head = 0;
-	return ((unsigned int)(dstlen + srclen));
+	return (dstlen + srclen);
 }
 
-int				ft_strlen(char *str)
+unsigned int	ft_strlen(char *str)
 {
-	int len;
+	unsigned int len;
 
 	len = 0;
 	while (*str)
