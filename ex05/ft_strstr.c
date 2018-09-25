75e657edba75eb4ed155a218e5dcd3c66ec10cf7
Sen Li <seli@e1z1r7p16.42.us.org>
2018-09-25 00:19:30 -0700

    fix strstr paran order

--- ex05/ft_strstr.c
+++ ex05/ft_strstr.c
@@ -6,7 +6,7 @@
 /*   By: seli <seli@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/24 19:25:28 by seli              #+#    #+#             */
-/*   Updated: 2018/09/24 23:54:47 by seli             ###   ########.fr       */
+/*   Updated: 2018/09/25 00:19:15 by seli             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,11 @@ char	*ft_strstr(char *str, char *to_find)
 {
 	if (!to_find || !str || !*to_find || !*str)
 		return (str);
-	while (*to_find)
+	while (*str)
 	{
 		if (ft_is_samestr(str, to_find))
-			return (to_find);
-		to_find++;
+			return (str);
+		str++;
 	}
 	return ((void *)0);
 }
